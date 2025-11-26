9730f7274bb2741162b83f402945d9bcd9e2a395
MaxAguero22 <jeezmax17@gmail.com>
2025-11-26 18:47:52 -0300

    add electron config

---
+++ public/electron.js
@@ -0,0 +1,30 @@
+import {app, BrowserWindow} from "electron"
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            contextIsolation: false,
+        },
+    });
+    win.loadURL('http://localhost:5173');
+};
+
+app.whenReady().then(() => {
+    createWindow();
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'win32') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
+
