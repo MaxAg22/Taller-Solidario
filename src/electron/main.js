961017447dbba5fc595f47afd4a77c9692aacae2
MaxAguero22 <jeezmax17@gmail.com>
2025-11-27 22:30:14 -0300

    fix on close window error and change logo

--- src/electron/main.js
+++ src/electron/main.js
@@ -1,36 +1,36 @@
-import {app, BrowserWindow} from "electron";
+import { app, BrowserWindow } from "electron";
 import path from "path";
 
 const createWindow = () => {
-    const win = new BrowserWindow({
-        width: 1280,
-        height: 720,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false,
-        },
-    });
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 720,
+    icon: path.join("public/prettylogo.png"),
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
 
-    if (app.isPackaged) {
-        win.loadFile(path.join(app.getAppPath(), 'dist/index.html'));
-    } else {
-        win.loadURL('http://localhost:5173');
-    }
+  if (app.isPackaged) {
+    win.loadFile(path.join(app.getAppPath(), "dist/index.html"));
+  } else {
+    win.loadURL("http://localhost:5173");
+  }
 };
 
 app.whenReady().then(() => {
+  createWindow();
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
+  }
 });
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'win32') {
-        app.quit();
-    }
-});
-
-app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-        createWindow();
-    }
-});
-
